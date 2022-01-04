44b410d4a98d02ff69dbe1047857c308b59efbdb
juyong <juyongjeong428@gmail.com>
2022-01-04 20:16:11 +0900

    프로그래머스 프린터 해쉬 문제 풀이

--- src/models/Programmers_Printer_Hash.java
+++ src/models/Programmers_Printer_Hash.java
@@ -1,5 +1,24 @@
-package models;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 
-public class Programmers_Printer_Hash {
-
+class Solution {
+    public int solution(int[] priorities, int location) {
+        int answer = 1;
+        PriorityQueue<Integer> queue = new PriorityQueue<>(Comparator.reverseOrder()); // priorities 값
+        for (int i : priorities) {
+            queue.offer(i);
+        }
+        while (!queue.isEmpty()) {
+            for (int i = 0; i < priorities.length; i++) {
+                if (priorities[i] == queue.peek()) {
+                    if (location == i) {
+                        return answer;
+                    }
+                    answer++;
+                    queue.poll();
+                }
+            }
+        }
+        return answer;
+    }
 }
