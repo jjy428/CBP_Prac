8adcd7fda13b9bc6d8825a05292bd56666f87622
juyong <juyongjeong428@gmail.com>
2022-01-26 20:16:51 +0900

    프로그래머스 오픈채팅

---
+++ src/models/Programmers_OpenChat.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class Solution {
+    public String[] solution(String[] record) {
+        ArrayList<String> arr = new ArrayList();
+		HashMap<String, String> map = new HashMap();
+		for (int i = 0; i < record.length; i++) {
+			String[] command = record[i].split(" ");
+
+			if (command[0].equals("Enter")) {
+				arr.add(command[1] + "님이 들어왔습니다.");
+				map.put(command[1], command[2]);
+			} else if (command[0].equals("Change")) {
+				map.put(command[1], command[2]);
+			} else {
+				arr.add(command[1] + "님이 나갔습니다.");
+			}
+		}
+		String[] answer = new String[arr.size()];
+		for (int i = 0; i < arr.size(); i++) {
+			int idx = arr.get(i).indexOf("님");
+			String id = arr.get(i).substring(0, idx);
+			String[] temp = arr.get(i).split(" ");
+			answer[i] = map.get(id) + "님이 " + temp[1];
+		}
+        return answer;
+    }
+}
