b67e71f1b905d23baae8c984788ce88ad5f0a7f2
juyong <juyongjeong428@gmail.com>
2022-02-01 23:00:52 +0900

    123

--- src/M/F_Refacoring/p25_receipt.java
+++ src/M/F_Refacoring/p25_receipt.java
@@ -12,7 +12,7 @@ public class p25_receipt {
 	  
 	    for (let perf of invoice.performances) {
 	      const play = plays[perf.playID];   
-	      let thisAmount = 0;
+	      let thisAmount = 0;  
 	  
 	      switch (play.type) {
 	      case "tragedy":
@@ -36,7 +36,7 @@ public class p25_receipt {
 	      volumeCredits += Math.max(perf.audience - 30, 0);
 	      // add extra credit for every ten comedy attendees
 	      if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
-	  
+	  s
 	      // print line for this order
 	      result += `  ${play.name}: ${format(thisAmount/100)} (${perf.audience} seats)\n`;
 	      totalAmount += thisAmount;
