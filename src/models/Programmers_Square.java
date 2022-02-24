555edaa1915b5472d917ee9ae7e3708e9a02879e
juyong <juyongjeong428@gmail.com>
2022-02-24 20:41:01 +0900

    1

---
+++ src/models/Programmers_Square.java
@@ -0,0 +1,11 @@
+lass Solution {
+    public long solution(long w, long h) {
+        long answer = 0;
+        
+        for(int i=0; i<w; i++) {
+            answer += h*i/w ;
+        }
+        
+        return answer * 2;
+    }
+}
